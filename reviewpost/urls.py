487615fd767daf6b44203998547689fa875bf6e1
tomo884 <tomohiro8841@hotmail.com>
2020-11-29 19:18:43 +0900

    Initial Commit

---
+++ reviewpost/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import signupview,loginview,sampleview,listview,detailview,CreateClass,logoutview,evaluationview
+
+urlpatterns = [
+    path('', listview,name='list'),
+    path('signup/', signupview,name='signup'),
+    path('login/', loginview,name='login'),
+    path('sample/', sampleview),
+    path('list/', listview,name='list'),
+    path('detail/<int:pk>/', detailview,name='detail'),
+    path('create/', CreateClass.as_view(),name='create'),
+    path('logout/',logoutview,name='logout'),
+    path('evaluation/<int:pk>/',evaluationview,name='evaluation'),
+]
+
